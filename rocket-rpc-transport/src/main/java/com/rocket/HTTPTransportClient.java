cdb55e885f5662ecfb7e184de6d9d456a477dc45
Panson <pansonpanson@163.com>
2020-03-14 18:34:49 +0800

    [网络模块] <add> 网络模块client

--- rocket-rpc-transport/src/main/java/com/rocket/HTTPTransportClient.java
+++ rocket-rpc-transport/src/main/java/com/rocket/HTTPTransportClient.java
@@ -1,8 +1,14 @@
 package com.rocket;
 
 import com.rocket.rpc.Peer;
+import org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * @author: Panson
@@ -10,15 +16,39 @@ import java.io.InputStream;
  */
 public class HTTPTransportClient implements TransportClient {
 
+	private String url;
+
 	@Override
 	public void connect(Peer peer) {
-
+		this.url = "http://" + peer.getHost() + peer.getPort();
 	}
 
 	@Override
 	public InputStream write(InputStream inputStream) {
-		return null;
+
+		try {
+			HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(url).openConnection();
+			httpURLConnection.setDoOutput(true);
+			httpURLConnection.setDoInput(true);
+			httpURLConnection.setUseCaches(false);
+			httpURLConnection.setRequestMethod("post");
+
+			httpURLConnection.connect();
+			IOUtils.copy(inputStream, httpURLConnection.getOutputStream());
+
+			int resultCode = httpURLConnection.getResponseCode();
+			if(resultCode == HttpURLConnection.HTTP_OK) {
+				return httpURLConnection.getInputStream();
+			} else {
+				return httpURLConnection.getErrorStream();
+			}
+
+		} catch (IOException e) {
+			throw new IllegalStateException();
+		}
+
 	}
+
 
 	@Override
 	public void close() {
